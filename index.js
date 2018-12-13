9403b2d5f1f0d709c85afcfae9543307db4115e2
Shawn Butterfield <sbutterfield@sbutterfie-ltm.internal.salesforce.com>
2018-12-13 12:49:29 -0800

    updaing host in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var httpProxy = require('http-proxy');
 var port = 8000;
 
 var routing = {
-  '/': { port: 80, host: sb-heroku-proxy01.herokuapp.com }
+  '/': { port: 80, host: 'sb-heroku-proxy01.herokuapp.com' }
 }
 
 var server = httpProxy.createServer(
